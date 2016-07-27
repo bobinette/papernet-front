a7854d649daf19d8e5006d4102ff05e3f9934fc9
bobinette <xavier.chassin@live.fr>
2016-07-28 00:46:23 +0200

    Add webpack prod config

---
+++ webpack-prod.config.js
@@ -0,0 +1,66 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'app'),
+    filename: 'bundle.js',
+    publicPath: '/app/',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel']
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }, {
+      test: /\.less$/,
+      loader: 'style!css!less'
+    }, {
+      test: /\.json$/,
+      loader: 'json'
+    },
+    {
+      test: /\.png$/,
+      loader: 'url-loader?limit=100000'
+    },
+    {
+      test: /\.jpg$/,
+      loader: 'file-loader'
+    },
+    {
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    },
+    {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/octet-stream'
+    },
+    {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file'
+    },
+    {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=image/svg+xml'
+    }]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+};
